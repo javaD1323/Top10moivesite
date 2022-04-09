e7ed970424cf2e8075ef7166408fe473634efc99
javaD1323 <j_pitt1111@yahoo.com>
2022-04-09 22:24:09 +0430

    update

---
+++ main.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+api_key = "39aee0a0cd694358cc2d7e65ddfccaa6"
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+db.create_all()
+
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+class AddMovie(FlaskForm):
+    title = StringField("Movie Title" , validators=[DataRequired()])
+    submit = SubmitField("Add")
+
+
+
+@app.route("/")
+def home():
+    # new_movie = Movie(
+    #     title="Phone Booth",
+    #     year=2002,
+    #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    #     rating=7.3,
+    #     ranking=10,
+    #     review="My favourite character was the caller.",
+    #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    # )
+    # db.session.add(new_movie)
+    # db.session.commit()
+    movie = db.session.query(Movie).all()
+    return render_template("index.html",  movies=movie)
+@app.route('/edit' , methods = ['POST', 'GET'])
+def edit():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    if form.validate_on_submit():
+        movie.rating = form.rating.data
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", form=form, movie=movie)
+
+@app.route('/delete')
+def delete():
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/add' ,methods = ['POST', 'GET'])
+def add():
+    form = AddMovie()
+    if form.validate_on_submit():
+        title = form.title.data
+        params = {
+            "api_key": api_key,
+            "query": title
+        }
+        respons = requests.get(MOVIE_DB_SEARCH_URL, params=params)
+        data = respons.json()["results"]
+        return render_template("select.html", data=data)
+    return  render_template("add.html", form=form)
+@app.route('/find')
+def find():
+    movie_id = request.args.get(id)
+    if movie_id :
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_id}"
+        response = requests.get(movie_api_url , params={"api_key": api_key, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("edit",  id=new_movie.id))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
